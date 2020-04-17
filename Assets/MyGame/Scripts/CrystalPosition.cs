86fdf9aa38e9d798cea1b9297c1a1e46a6625ea4
aschnee <alina.schneeberger@htl-salzburg.ac.at>
2020-04-17 11:49:42 +0200

    added throwable script to crystals, fixed tag checking

--- Assets/MyGame/Scripts/CrystalPosition.cs
+++ Assets/MyGame/Scripts/CrystalPosition.cs
@@ -1,19 +1,17 @@
 ﻿using UnityEngine;
 
-
-
 [RequireComponent(typeof(Collider))]
 public class CrystalPosition : MonoBehaviour
 {
     [Range(0, 2)]
     public int correspondingCrystalIndex = 0;
 
-
-
     private void OnTriggerEnter(Collider col)
     {
+        // checking which crystal is entering the collider
+        if (!col.CompareTag("c" + correspondingCrystalIndex.ToString())) return;
 
-
-
+        // moving the crystal to its dedicated position
+        col.transform.position = this.transform.position;
     }
 }
